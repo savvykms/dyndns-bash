a58e0275e8f27139abef62fbd9387f2c3894c598
Savvy <savvykms@gmail.com>
2021-06-03 23:54:57 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # dyndns-bash
+
+This codebase consists of a Bash shell script with which to update a specified DNS A (alias) record contained within a specified AWS Route53 DNS zone with an optionally specified TTL (time-to-live) value (in seconds).
+
+# Requirements
+
+## Software dependencies
+
+Rough list of software required:
+ - Bash
+ - curl
+ - AWS CLI version 1
+
+This was developed with the following versions initially:
+ - GNU bash, version 4.4.20(1)-release (x86_64-pc-linux-gnu)
+ - curl 7.58.0 (x86_64-pc-linux-gnu) libcurl/7.58.0 OpenSSL/1.1.1i zlib/1.2.11 libidn2/2.3.0 libpsl/0.19.1 (+libidn2/2.0.4) nghttp2/1.30.0 librtmp/2.3
+ - OpenSSL 1.1.1i  8 Dec 2020
+ - aws-cli/1.14.44 Python/3.6.9 Linux/4.4.0-19041-Microsoft botocore/1.16.19
+
+## AWS permissions
+
+The AWS principal(s) being used must have the `route53:ChangeResourceRecordSets` permissions for any DNS zones you wish to modify.
+
+Example IAM Policy:
+```
+{
+   "Version": "2012-10-17",
+   "Statement": [
+       {
+           "Sid": "allow-dyndns-bash",
+           "Effect": "Allow",
+           "Action": "route53:ChangeResourceRecordSets",
+           "Resource": "arn:aws:route53:::hostedzone/<YOUR_ZONE_ID_HERE>"
+       }
+   ]
+}
+```
+
+# Invocation
+
+```
+bash dyndns.bash "XYZROUTE53ZONEID" "test.example.com"
+```
+
+# Configuration
+
+To provide AWS credentials, use environment or file-driven AWS CLI configuration as documented by Amazon.
---
+++ dyndns.bash
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------#
+#
+#   Created by savvykms; https://github.com/savvykms
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#------------------------------------------------------------------------------#
+
+HOSTED_ZONE_ID="$1"
+RECORD_NAME="$2"
+RECORD_TTL="${3:-300}"
+
+RECORD_TYPE="A"
+RECORD_NAME="${RECORD_NAME}."
+
+#------------------------------------------------------------------------------#
+
+function error_msg()
+{
+  >&2 echo "$@"
+}
+
+function valid_ip()
+{
+IP=$1
+  if [[ $IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+    return 0;
+  else
+    return 1;
+  fi
+}
+
+function ip_aws()
+{
+  curl -s "https://checkip.amazonaws.com/"
+}
+function ip_ipify()
+{
+  curl -s "https://api.ipify.org?format=text"
+}
+function ip_myipio()
+{
+  curl -s "https://api.my-ip.io/ip.txt"
+}
+
+declare -a IP_FUNCS
+IP_FUNCS[1]="ip_aws"
+IP_FUNCS[2]="ip_ipify"
+IP_FUNCS[3]="ip_myipio"
+
+#------------------------------------------------------------------------------#
+
+if [[ -z "${HOSTED_ZONE_ID}" ]]; then
+  error_msg "Error: no zone specified!"
+  exit 1
+fi
+
+if [[ -z "${RECORD_NAME}" ]]; then
+  error_msg "Error: no record name specified!"
+  exit 1
+fi
+
+#Screw validating record names and TTLs
+#Edge cases such as:
+# - specifying the proper name format of "test.example.com." with trailing period
+# - non-integer ttl values
+# - invalid zone IDs
+# - random keyboard mashing
+#are not validated
+
+#------------------------------------------------------------------------------#
+
+MY_IP=
+
+for FUNC_IND in "${!IP_FUNCS[@]}";
+do
+  ip=$(${IP_FUNCS[$FUNC_IND]})
+  if valid_ip $ip;
+  then
+    MY_IP=$ip
+    break;
+  else
+    error_msg "Error obtaining ip using ${IP_FUNCS[$FUNC_IND]}"
+    error_msg "response: $ip"
+  fi
+done
+
+echo "Setting IP to \"$MY_IP\"";
+
+aws route53 change-resource-record-sets --hosted-zone-id "${HOSTED_ZONE_ID}" --change-batch "$(cat <<-EEOOFF
+{
+  "Comment":"Updated by $0 at $(date +"%s")",
+  "Changes":[
+    {
+      "Action":"UPSERT",
+      "ResourceRecordSet":{
+        "ResourceRecords":[
+          {
+            "Value":"${MY_IP}"
+          }
+        ],
+        "Name":"${RECORD_NAME}",
+        "Type":"${RECORD_TYPE}",
+        "TTL":${RECORD_TTL}
+      }
+    }
+  ]
+}
+EEOOFF
+)"
